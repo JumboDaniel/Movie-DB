f6e2c2da5bf2cd57738d1973a4deb4d13f3fd8e7
Jumbo Daniel <jumbodaniel15@gmail.com>
2022-10-02 20:14:08 +0800

    Added the progress bar on route change Edited the _app.tsx file Installed the Nprogress bar Installed the Nprogress types Used Useeffect to show the progress bar when the components are mounted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,43 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import {useEffect } from "react";
+import NProgress from "nprogress";
+import Head from "next/head";
+//File Import
 import ThemeContextWrapper from "../context/ThemeContextWrapper";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  NProgress.configure({showSpinner: false});
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (url) => {
+      console.log(`routeChangeStart ${url}`);
+      NProgress.start();
+    });
+
+    Router.events.on("routeChangeComplete", (url) => {
+      console.log(NProgress.done(true));
+
+      NProgress.done();
+    });
+  }, [Router]);
+
   return (
-    <ThemeContextWrapper>
-      <Component {...pageProps} />
-    </ThemeContextWrapper>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        ;
+      </Head>
+      <ThemeContextWrapper>
+        <Component {...pageProps} />
+      </ThemeContextWrapper>
+    </>
   );
 }
 
